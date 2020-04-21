7cf20f9336863323fd96b31df4b17329a6fea19b
sama3612 <sama3612@colorado.edu>
2020-04-21 16:37:49 -0600

    Should be all good

--- Link_project/server.js
+++ Link_project/server.js
@@ -1,6 +1,5 @@
 /***********************
   Load Components!
-
   Express      - A Node.js Framework
   Body-Parser  - A tool to help use parse the data in a post request
   Pg-Promise   - A database tool to help use connect to our PostgreSQL database
@@ -43,29 +42,24 @@ app.use(express.static(__dirname + '/'));//This line is necessary for us to use 
    - Database access
    - Parse parameters from get (URL) and post (data package)
    - Render Views - This will decide where the user will go after the get/post request has been processed
-
  Web Page Requests:
-
   Login Page:        Provided For your (can ignore this page)
   Registration Page: Provided For your (can ignore this page)
   Home Page:
   		/home - get request (no parameters) 
   				This route will make a single query to the favorite_colors table to retrieve all of the rows of colors
   				This data will be passed to the home view (pages/home)
-
   		/home/pick_color - post request (color_message)
   				This route will be used for reading in a post request from the user which provides the color message for the default color.
   				We'll be "hard-coding" this to only work with the Default Color Button, which will pass in a color of #FFFFFF (white).
   				The parameter, color_message, will tell us what message to display for our default color selection.
   				This route will then render the home page's view (pages/home)
-
   		/home/pick_color - get request (color)
   				This route will read in a get request which provides the color (in hex) that the user has selected from the home page.
   				Next, it will need to handle multiple postgres queries which will:
   					1. Retrieve all of the color options from the favorite_colors table (same as /home)
   					2. Retrieve the specific color message for the chosen color
   				The results for these combined queries will then be passed to the home view (pages/home)
-
   		/team_stats - get request (no parameters)
   			This route will require no parameters.  It will require 3 postgres queries which will:
   				1. Retrieve all of the football games in the Fall 2018 Season
@@ -74,7 +68,6 @@ app.use(express.static(__dirname + '/'));//This line is necessary for us to use 
   			The three query results will then be passed onto the team_stats view (pages/team_stats).
   			The team_stats view will display all fo the football games for the season, show who won each game, 
   			and show the total number of wins/losses for the season.
-
   		/player_info - get request (no parameters)
   			This route will handle a single query to the football_players table which will retrieve the id & name for all of the football players.
   			Next it will pass this result to the player_info view (pages/player_info), which will use the ids & names to populate the select tag for a form 
@@ -91,18 +84,21 @@ app.get('/login', function(req, res) {
 	});
 });
 
-// registration page 
-app.get('/register', function(req, res) {
-	res.render('pages/register',{
-		my_title:"Registration Page"
-	});
-});
 
 function validUser() {
   return true;
 }
 
-// signin
+// registration page
+app.get('/register', function(req, res) {
+	res.render('pages/register',{
+		local_css:"signin.css",
+	    my_title:"Registration Page",
+	    big_failure: 0
+	});
+});
+
+// signup
 app.post('/login/signin', function(req, res) {
   inputEmail = req.body.inputEmail;
   var inputPassword =  req.body.inputPassword;
@@ -202,6 +198,64 @@ app.post('/addFriends/add', function(req, res) {
 		})
 	    }
 	})
+});
+
+function validUserForCreation(fName,lName,email,password,cPassword) {
+	if(password != cPassword) {//case 1
+		return 1;
+	}
+	//check everything else
+	return 0;
+}
+
+//register signup
+app.post('/register/signup', function(req, res) {
+	var firstName = req.body.firstName;
+	var lastName = req.body.lastName;
+	var inputEmail = req.body.inputEmail;
+	var inputPassword =  req.body.inputPassword;
+	var inputCPassword = req.body.inputCPassword;
+	var validUser = validUserForCreation(firstName, lastName, inputEmail, inputPassword, inputCPassword);
+
+	if(validUser == 1) {//looks like it failed...
+		res.render('pages/register',{
+			local_css:"signin.css",
+			my_title:"Registration Page",
+			big_failure: 1
+		});
+	}
+
+	var query = 'insert into users (first_name, last_name, email, password)'
+	+ 'values (\''+ firstName + '\', \''+ lastName + '\', \''+ inputEmail + '\', \''+ inputPassword + '\');';
+
+	db.any(query)
+		.then(function (rows) {
+			console.log("Authenticated..");
+              res.render('pages/home',{
+                my_title: 'Home Page',
+                data: '',
+                color: '',
+                color_msg: '',
+                data:rows
+              })
+	})
+	.catch(function (err) {
+		// display error message in case an error
+		console.log('error', err);
+		res.render('pages/home',{
+			my_title: "error",
+			data: '',
+			color: '',
+			color_msg: ''
+			})
+		})
+  });
+
+//get Home
+app.get('/home', function(req, res) {
+  res.render('pages/home',{
+    my_title: "Home Page"
+  })
 });
 
 app.listen(3000);
